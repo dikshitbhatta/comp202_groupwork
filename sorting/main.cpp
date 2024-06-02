7abd77b62796f21233e7b3684b5bfdff82d72b66
dikshitbhatta <147680213+dikshitbhatta@users.noreply.github.com>
2024-06-02 10:33:02 +0545

    updated

--- sorting/main.cpp
+++ sorting/main.cpp
@@ -1,15 +1,30 @@
 #include<iostream>
+#include <random>
 #include"insertionsort.cpp"
 using namespace std;
 
 int main() {
     const int length = 8;
-    int* ptr;
-    int arr[length] = {4, 7, 5, 9, 0, 2, 8, 6};
-    ptr=insertionsort(arr,length);
-     
+    int arr[length];
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 999);
+    
+    cout << "Randomly Generated Array: "<<endl;
     for (int i = 0; i < length; i++) {
-        cout << ptr[i] << endl;
+        arr[i] = dis(gen);
+        cout << arr[i] << " ";
     }
+    cout << endl;
+    
+    int* ptr = insertionsort(arr, length);
+    
+    cout << "Sorted Array: "<<endl;
+    for (int i = 0; i < length; i++) {
+        cout << ptr[i] << " ";
+    }
+    cout << endl;
+    
     return 0;
 }
