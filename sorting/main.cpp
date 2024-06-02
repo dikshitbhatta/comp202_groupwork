2d65a6c4ae050aabde190b2e737f0c4556aac6ba
dikshitbhatta <147680213+dikshitbhatta@users.noreply.github.com>
2024-06-02 10:52:27 +0545

    time complexity

--- sorting/main.cpp
+++ sorting/main.cpp
@@ -1,19 +1,23 @@
 #include<iostream>
+#include<ctime>
 #include <random>
 #include"insertionsort.cpp"
 using namespace std;
 
 int main() {
+    clock_t start,end;
+    double timecomplexity;
+    start=clock();
     const int length = 8;
     int arr[length];
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 999);
+    std::uniform_int_distribution<> rand(0, 99);
     
     cout << "Randomly Generated Array: "<<endl;
     for (int i = 0; i < length; i++) {
-        arr[i] = dis(gen);
+        arr[i] = rand(gen);
         cout << arr[i] << " ";
     }
     cout << endl;
@@ -25,6 +29,9 @@ int main() {
         cout << ptr[i] << " ";
     }
     cout << endl;
-    
+    end=clock();
+
+    timecomplexity=((double)(end-start))/CLOCKS_PER_SEC;
+    cout<<timecomplexity<<endl;   
     return 0;
 }
